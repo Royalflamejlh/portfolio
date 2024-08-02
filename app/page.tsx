00c210fa716ad656acd16c3d04ec914c4fc5b06d
Rooyal <49204095+Royalflamejlh@users.noreply.github.com>
2024-08-02 16:33:31 -0400

    Fixed the highlighted projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ProjectPosts } from 'app/components/posts'
+import Link from 'next/link'
 
 export default function Page() {
   return (
@@ -23,13 +24,46 @@ export default function Page() {
         />
       </div>
       
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
         Highlighted Projects
       </h1>
       
-      <div className="my-8">
-        <p> My Chess Engine</p>
+      <div className="mb-8">
+        <Link
+          key="craig"
+          className="flex flex-col space-y-1 mb-4"
+          href={`/projects/craig`}
+        >
+          <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              {"Craig the Chess Engine"}
+            </p>
+          </div>
+        </Link>
+        <Link
+          key="xpvp"
+          className="flex flex-col space-y-1 mb-4"
+          href={`/projects/xpvp`}
+        >
+          <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              {"XPVP - Competitive Minecraft"}
+            </p>
+          </div>
+        </Link>
+        <Link
+          key="watt-balance"
+          className="flex flex-col space-y-1 mb-4"
+          href={`/projects/watt-balance`}
+        >
+          <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              {"Kibble Watt Balance"}
+            </p>
+          </div>
+        </Link>
       </div>
+
     </section>
   )
 }
