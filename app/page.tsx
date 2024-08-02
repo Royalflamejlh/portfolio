b97846a242d5ad86fec646332e7ced9a47865cab
Rooyal <49204095+Royalflamejlh@users.noreply.github.com>
2024-08-02 12:09:10 -0400

    Added favicon and images

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,30 @@ import { ProjectPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
+      <div className="flex items-start mb-8">
+        <div className="flex-grow">
+          <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
+            About Me
+          </h1>
+          <p>
+            {`Hello! I'm John, a recent computer engineering graduate from 
+            the University of Illinois and this is my little website documenting
+            and showcasing my projects.`}
+          </p>
+        </div>
+        
+        <img
+          src="/static/images/john.png"
+          alt="John"
+          className="ml-4 rounded-lg"
+          style={{ width: '150px', height: 'auto' }} // Adjust width as needed
+        />
+      </div>
+      
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        About Me
+        Highlighted Projects
       </h1>
-      <p className="mb-4">
-        {`Hello! Im John, a recent computer engineering graduate from 
-       University of Illinois and this is my little website documenting
-       and showcasing my projects.`}
-      </p>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Checkout my projects
-      </h1>
+      
       <div className="my-8">
         <ProjectPosts />
       </div>
